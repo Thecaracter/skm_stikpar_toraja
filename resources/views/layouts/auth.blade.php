e768276cbd063b97d0f9f0c9fe0404bfee828390
Rizqi Nur Andi Putra <e31211961@student.polije.ac.id>
2024-11-12 18:38:27 +0700

    first commit

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }} - @yield('title')</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="antialiased">
+    @yield('content')
+    @stack('scripts')
+</body>
+
+</html>
